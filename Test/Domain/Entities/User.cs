f223943719e1b95e99b0279c1303a9ce701dcb93
vupham04112002 <vupham2002@outlook.com>
2024-06-14 22:11:51 +0700

    update package name

---
+++ Test/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Core.Domain.Entities;
+using Shared.Core.Domain.Validators;
+
+namespace Test.Domain.Entities;
+
+public class User : BaseEntity
+{
+    
+}
+
+public class UserRule : BaseNoParamValidator<User, UserRule>
+{
+    public UserRule()
+    {
+    }
+}
