7215aa3a3cc78087e679c61dd7019f03b4dbe6af
vupham04112002 <vupham2002@outlook.com>
2024-06-21 17:15:38 +0700

    add AddAsync to IBaseRepository

--- Shared.Core/Drivens/Persistence/Repositories/IBaseRepository.cs
+++ Shared.Core/Drivens/Persistence/Repositories/IBaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using Shared.Core.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Shared.Core.Domain.Entities;
 
 namespace Shared.Core.Drivens.Persistence.Repositories;
 
@@ -7,4 +8,6 @@ public interface IBaseRepository<TEntity> where TEntity : BaseEntity
     Task<TEntity?> GetByIdAsync(string id);
 
     Task<List<TEntity>> GetAllAsync();
+
+    ValueTask<EntityEntry<TEntity>> AddAsync(TEntity entity);
 }
