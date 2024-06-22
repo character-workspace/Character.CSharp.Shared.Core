0bf8944bc2bc9d32f5c724fe0f391064ff60d205
vupham04112002 <vupham2002@outlook.com>
2024-06-22 15:23:39 +0700

    add ErrorResponse

---
+++ Shared.Core/Drivings/Models/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Core.Drivings.Models;
+
+public class Error
+{
+    public required string Field { get; init; }
+    public required dynamic? AttemptData { get; init; }
+    public required string ErrorCode { get; init; }
+}
---
+++ Shared.Core/Drivings/Models/ErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace Shared.Core.Drivings.Models;
+
+public class ErrorResponse
+{
+    public required List<KeyValuePair<string, Error>> Errors { get; set; }
+
+    public static ErrorResponse ToErrorResponse(ValidationResult validationResult)
+    {
+        return new ErrorResponse
+        {
+            Errors = validationResult.Errors.Select(failure 
+                => new KeyValuePair<string, Error>(failure.PropertyName, new Error
+                {
+                    Field = failure.PropertyName,
+                    AttemptData = failure.AttemptedValue,
+                    ErrorCode = failure.ErrorCode
+                })
+            ).ToList()
+        };
+    }
+}
