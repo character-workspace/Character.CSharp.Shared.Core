8f291e0dc5730a9a242e9e917829481ac947096b
vupham04112002 <vupham2002@outlook.com>
2024-10-05 12:23:34 +0700

    test | add unittests for RequiredGuid and MaxLengthString

---
+++ Tests/UnitTest/Domain/Rules/Base/GuidRules/RequiredGuidTest.cs
@@ -0,0 +1,76 @@
+﻿using Shared.Core.Domain.Errors.Base;
+using Shared.Core.Domain.Rules.Base.GuidRules;
+
+namespace UnitTest.Domain.Rules.Base.GuidRules;
+
+public class RequiredGuidTest
+{
+    #region Unhappy
+
+    [Fact]
+    public void Validate_ShouldReturnError_WhenGuidIsEmpty()
+    {
+        #region Arrange
+        
+        // input
+        var guidInput = Guid.Empty;
+
+        // target
+        var targetTest = new RequiredGuid();
+        
+        // expected
+        var expectedErrorCode = CommonErrors.Required;
+        var expectedErrorCount = 1;
+
+        #endregion
+
+        #region Act
+        var result = targetTest.Validate(guidInput);
+        #endregion
+        
+        #region Assert
+
+        var errors = result.Errors;
+        
+        Assert.Equal(expectedErrorCount, errors.Count);
+        Assert.Equal(expectedErrorCode, errors[0].ErrorCode);
+
+        #endregion
+    }
+
+    #endregion
+
+    #region Happy
+
+    [Fact]
+    public void Validate_ShouldSuccess_WhenGuidIsValid()
+    {
+        #region Arrange
+    
+        // mock
+        
+        // input
+        var guidInput = Guid.NewGuid();
+        
+        // target
+        var targetTest = new RequiredGuid();
+        
+        // expected
+        var expectedErrorCount = 0;
+        
+        #endregion
+    
+        #region Act
+        var result = targetTest.Validate(guidInput);
+        #endregion
+    
+        #region Assert
+        
+        var errors = result.Errors;
+        Assert.Equal(expectedErrorCount, errors.Count);
+        
+        #endregion
+    }
+
+    #endregion
+}
---
+++ Tests/UnitTest/Domain/Rules/Base/StringRules/MaxLengthStringTest.cs
@@ -0,0 +1,105 @@
+﻿using Shared.Core.Domain.Errors.Base;
+using Shared.Core.Domain.Rules.Base.StringRules;
+
+namespace UnitTest.Domain.Rules.Base.StringRules;
+
+public class MaxLengthStringTest
+{
+    [Fact]
+    public void Validate_ShouldReturnError_WhenStringExceedsMaxLength()
+    {
+        #region Arrange
+        
+        // dependency
+        var maxLength = 5;
+
+        // input
+        var stringInput = "1234567890";
+        
+        // target
+        var targetTest = new MaxLengthString(maxLength);
+        
+        // expected
+        var expectedErrorCount = 1;
+        var expectedErrorCode = StringErrors.ExceedMaxLength;
+
+        #endregion
+        
+        #region Act
+        var result = targetTest.Validate(stringInput);
+        #endregion
+
+        #region Assert
+        
+        var errors = result.Errors;
+        
+        Assert.Equal(expectedErrorCount, errors.Count);
+        Assert.Equal(expectedErrorCode, errors[0].ErrorCode);
+        
+        #endregion
+    }
+    
+    [Fact]
+    public void Validate_ShouldSuccess_WhenStringEqualsMaxLength()
+    {
+        #region Arrange
+
+        // dependency
+        var maxLength = 5;
+        
+        // input
+        var stringInput = "12345";
+        
+        // target
+        var targetTest = new MaxLengthString(maxLength);
+        
+        // expected
+        var expectedErrorCount = 0;
+
+        #endregion
+        
+        #region Act
+        var result = targetTest.Validate(stringInput);
+        #endregion
+
+        #region Assert
+        
+        var errors = result.Errors;
+        
+        Assert.Equal(stringInput.Length, maxLength);
+        Assert.Equal(expectedErrorCount, errors.Count);
+        
+        #endregion
+    }
+    
+    [Fact]
+    public void Validate_ShouldSuccess_WhenStringIsEmpty()
+    {
+        #region Arrange
+        
+        // dependency
+        var maxLength = 5;
+        
+        // input
+        var stringInput = string.Empty;
+        
+        // target
+        var targetTest = new MaxLengthString(maxLength);
+        
+        // expected
+        var expectedErrorCount = 0;
+
+        #endregion
+        
+        #region Act
+        var result = targetTest.Validate(stringInput);
+        #endregion
+
+        #region Assert
+        
+        var errors = result.Errors;
+        Assert.Equal(expectedErrorCount, errors.Count);
+        
+        #endregion
+    }
+}
